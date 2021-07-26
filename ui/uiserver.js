ed17ffcfebe6f4e7617ca3b1daf0fc77acc43a34
gavindang2911 <hoaidang2999@gmail.com>
2021-07-26 11:38:35 -0700

    Browser History Router

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const proxy = require('http-proxy-middleware');
 
@@ -6,7 +7,7 @@ const app = express();
 
 const enableHMR = (process.env.ENABLE_HMR || 'true') === 'true';
 
-if (enableHMR && (process.env.NODE_ENV !== 'production')) {
+if (enableHMR && process.env.NODE_ENV !== 'production') {
   console.log('Adding dev middlware, enabling HMR');
   /* eslint "global-require": "off" */
   /* eslint "import/no-extraneous-dependencies": "off" */
@@ -24,6 +25,9 @@ if (enableHMR && (process.env.NODE_ENV !== 'production')) {
   app.use(hotMiddleware(compiler));
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
 app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
@@ -31,8 +35,8 @@ if (apiProxyTarget) {
   app.use('/graphql', proxy({ target: apiProxyTarget }));
 }
 
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
-  || 'http://localhost:3000/graphql';
+const UI_API_ENDPOINT =
+  process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
 
 app.get('/env.js', (req, res) => {
