091444a18ef6f5a61134bce327f3da1b831d71c1
gavindang2911 <hoaidang2999@gmail.com>
2021-07-28 11:23:02 -0700

    deleted issue api

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -13,6 +13,7 @@
 /* eslint no-restricted-globals: "off" */
 
 db.issues.remove({});
+db.deleted_issues.remove({});
 
 const issuesDB = [
   {
@@ -23,6 +24,15 @@ const issuesDB = [
     created: new Date('2019-01-15'),
     due: undefined,
     title: 'Error in console when clicking Add',
+    description:
+      'Steps to recreate the problem:' +
+      '\n1. Refresh the browser.' +
+      '\n2. Select "New" in the filter' +
+      '\n3. Refresh the browser again. Note the warning in the console:' +
+      '\n   Warning: Hash history cannot PUSH the same path; a new entry' +
+      '\n   will not be added to the history stack' +
+      '\n4. Click on Add.' +
+      "\n5. There is an error in console, and add doesn't work.",
   },
   {
     id: 2,
@@ -32,6 +42,9 @@ const issuesDB = [
     created: new Date('2019-01-16'),
     due: new Date('2019-02-01'),
     title: 'Missing bottom border on panel',
+    description:
+      'There needs to be a border in the bottom in the panel' +
+      ' that appears when clicking on Add',
   },
 ];
 
@@ -46,3 +59,5 @@ db.issues.createIndex({ id: 1 }, { unique: true });
 db.issues.createIndex({ status: 1 });
 db.issues.createIndex({ owner: 1 });
 db.issues.createIndex({ created: 1 });
+
+db.deleted_issues.createIndex({ id: 1 }, { unique: true });
